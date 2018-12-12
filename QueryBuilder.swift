481e570cfbdce47f6f9d66d2eb0ad571026c9d07
dpabril <don.abril09@gmail.com>
2018-12-13 01:13:57 +0800

    Added query statements

--- QueryBuilder.swift
+++ QueryBuilder.swift
@@ -1,77 +1,47 @@
-import GRDB
+class PreparedStatements {
+    static var selectQRTag = """
+        SELECT *
+        FROM QRTag
+        WHERE url = ?
+        """
 
-class QueryBuilder {
-    static func fetchBuilding(_ alias : String) -> Building? {
-        var building : Building?
-        do {
-            try DB.write { db in
-                building = try Building.fetchOne(db, "SELECT * FROM Building WHERE alias = ?", arguments: [alias])
-                return building
-            }
-        } catch {
-            print(error)
-        }
-    }
+    static var selectBuilding = """
+        SELECT *
+        FROM Building
+        WHERE alias = ?
+        """
 
     // CS 199, for GPS maybe?
-    static func fetchAllBuildings() -> [Building] {
-        var buildings : [Building]
-        do {
-            try DB.write { db in
-                buildings = try Building.fetchAll(db, "SELECT alias, name FROM Building")
-                return buildings
-            }
-        } catch {
-            print(error)
-        }
-    }
+    static var selectAllBuildings = """
+        SELECT *
+        FROM Building
+        """
 
-    static func fetchFloor(_ bldg : String, _ level : Int) -> Floor? {
-        var floor : Floor?
-        do {
-            try DB.write { db in
-                floor = try Floor.fetchOne(db, "SELECT * FROM Floor WHERE (bldg, level) = (?, ?)", arguments: [bldg, level])
-                return floor
-            }
-        } catch {
-            print(error)
-        }
-    }
+    static var selectFloor = """
+        SELECT *
+        FROM Floor
+        WHERE bldg = ?
+          AND level = ?
+        """
 
-    static func fetchBuildingFloors(_ bldg : String) -> [Floor] {
-        var floors : [Floor]
-        do {
-            try DB.write { db in
-                floors = try Floor.fetchAll(db, "SELECT * FROM Floor WHERE bldg = ?", arguments: [bldg])
-                return floors
-            }
-        } catch {
-            print(error)
-        }
-    }
+    static var selectFloors = """
+        SELECT *
+        FROM Floor
+        WHERE bldg = ?
+        """
 
-    static func fetchIndoorLocation(_ bldg : String, _ level : Int, _ name : String) -> IndoorLocation? {
-        var loc : IndoorLocation?
-        do {
-            try DB.write { db in
-                loc = try IndoorLocation.fetchOne(db, "SELECT * FROM IndoorLocation WHERE (bldg, level, name) = (?, ?, ?)", arguments: [bldg, level, name])
-                return loc
-            }
-        } catch {
-            print(error)
-        }
-    }
+    static var selectIndoorLocation = """
+        SELECT *
+        FROM IndoorLocation
+        WHERE bldg = ?
+          AND level = ?
+          AND name = ?
+        """
 
-    static func fetchFloorIndoorLocations(_ bldg : String, _ level : Int) -> [IndoorLocation] {
-        var locs : [IndoorLocation]
-        do {
-            try DB.write { db in
-                locs = try IndoorLocation.fetchAll(db, "SELECT * FROM IndoorLocation WHERE (bldg, level) = (?, ?)", arguments: [bldg, level])
-                return locs
-            }
-        } catch {
-            print(error)
-        }
-    }
-
+    static var selectIndoorLocations = """
+        SELECT *
+        FROM IndoorLocation
+        WHERE bldg = ?
+          AND level = ?
+        """
 }
